a85403cc9f525957d7b0b8b55b779193aa0b39be
2814109 <2814109k@gmail.com>
2022-10-09 10:44:06 +0900

    setting for hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
